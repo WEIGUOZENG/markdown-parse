078ae0440c4b4eac0c0d6f26d28519566b3c0f03
WEIGUOZENG <w3zeng@ucsd.edu>
2022-01-31 14:35:42 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,21 +12,39 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(nextOpenBracket == -1){
+                break;
+            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            if(nextOpenBracket == -1){
+                break;
+            }
             int openParen = markdown.indexOf("(", nextCloseBracket);
+            if(nextOpenBracket == -1){
+                break;
+            }
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(nextOpenBracket == -1){
+                break;
+            }
+            
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
+            if(openParen - nextCloseBracket == 1){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+
+
+            /*System.out.println(currentIndex);
             System.out.println("index of openParen: " + openParen);
             System.out.println("index of closeParen: " + closeParen);
             System.out.println("index of openBracket: " + nextOpenBracket);
             System.out.println("index of closeBracket: " + nextCloseBracket);
+            */
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of("test-file.md");//Path.of(args[0]);
+		Path fileName = Path.of("test-file4.md");//Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
